00ea9c1dc29f72ef50d3abb24a0fed985b0422a4
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-08-05 16:38:31 +0100

    little change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+function App() {
+  return <h1>hello </h1>;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
