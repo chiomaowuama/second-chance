0ae68197cb411f63a7e659590f2ec5236df2f782
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-09-10 14:00:28 +0100

    still following

---
+++ new--react/src/component/profilecard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class profilecard extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>hello profile card</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default profilecard;
